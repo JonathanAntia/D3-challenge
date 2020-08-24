135006d3b453c3a986ca1db53691af82e5e657b8
JonathanAntia <a_jon12@yahoo.com>
2020-08-23 22:17:34 -0500

    javascript app

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,66 +21,79 @@ const svg = d3.select('.article')
 const chartGroup = svg.append('g')
                     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-// Step 3: Import data from the csv file D3.csv().then(){}
+// Step 3: Import data from the csv file
 d3.csv('assets/data/data.csv').then(function(healthData){
-    // Step 4: Parse the data and convert to numerical
-    healthData.forEach(d => {
+        // Step 4: Parse the data and convert to numerical
+        healthData.forEach(d => {
         d.poverty = +d.poverty;
         d.healthcare = +d.healthcare;
-    });
+        });
 
-// Step 5: Create linear scaling functions for x and y
-const xScale = d3.scaleLinear()
-                .domain([d3.min(healthData, d => d.poverty)-1,d3.max(healthData, d => d.poverty)+1])
-                .range([0, chartWidth]);
+        // Step 5: Create linear scaling functions for x and y
+        const xScale = d3.scaleLinear()
+                        .domain([d3.min(healthData, d => d.poverty)-1,d3.max(healthData, d => d.poverty)+1])
+                        .range([0, chartWidth]);
 
-const yScale = d3.scaleLinear()
-                .domain([d3.min(healthData, d => d.healthcare)-0.5,d3.max(healthData, d => d.healthcare)+2])
-                .range([chartHeight, 0]);
+        const yScale = d3.scaleLinear()
+                        .domain([d3.min(healthData, d => d.healthcare)-0.5,d3.max(healthData, d => d.healthcare)+2])
+                        .range([chartHeight, 0]);
 
-// Step 6: Create axes functions
-const bottomAxis = d3.axisBottom(xScale),
-        leftAxis = d3.axisLeft(yScale);
+        // Step 6: Create axes functions
+        const bottomAxis = d3.axisBottom(xScale),
+                leftAxis = d3.axisLeft(yScale);
 
-// Step 7: Append the axes to the chart group
-chartGroup.append('g')
-        .attr('transform', `translate(0, ${chartHeight})`)
-        .call(bottomAxis);
+        // Step 7: Append the axes to the chart group
+        chartGroup.append('g')
+                .attr('transform', `translate(0, ${chartHeight})`)
+                .call(bottomAxis);
 
-chartGroup.append('g')
-        .call(leftAxis);
+        chartGroup.append('g')
+                .call(leftAxis);
 
-// Step 8: Create circles
-const circleGroup = chartGroup.selectAll('circle')
-                            .data(healthData)
-                            .enter()
-                            .append('circle')
-                            .attr('cx', d => xScale(d.poverty))
-                            .attr('cy', d => yScale(d.healthcare))
-                            .attr('r', '10')
-                            .classed('stateCircle', true);
+        // Step 8: Create circles
+        const circleGroup = chartGroup.selectAll('circle')
+                                .data(healthData)
+                                .enter()
+                                .append('circle')
+                                .attr('cx', d => xScale(d.poverty))
+                                .attr('cy', d => yScale(d.healthcare))
+                                .attr('r', '10')
+                                .classed('stateCircle', true);
 
-// Step 9: Include state abbreviations in the circles
-const stateText = chartGroup.selectAll('text')
-                            .data(healthData)
-                            .enter()
-                            .append('text')
-                            .attr('dx', d => xScale(d.poverty))
-                            .attr('dy', d => yScale(d.healthcare))
-                            .text(d => d.abbr)
-                            .classed('stateText', true);
-// Step 10: Create axes labels
-chartGroup.append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 0 - margin.left + 15)
-        .attr('x', 0 - (chartHeight/2))
-        .classed('aText', true)
-        .text('Lacks Healthcare (%)');
+        // Step 9: Include state abbreviations in the circles
+        // const stateText = chartGroup.selectAll('text')
+        //                         .data(healthData)
+        //                         .enter()
+        //                         .append('text')
+        //                         .attr('x', d => xScale(d.poverty))
+        //                         .attr('y', d => yScale(d.healthcare))
+        //                         .text(d => d.abbr)
+        //                         .classed('stateText', true);
+        // console.log(stateText);
+        var circleLabels = chartGroup.selectAll(null)
+        .data(healthData)
+        .enter()
+        .append("text")
+        .attr('x', d => xScale(d.poverty))
+        .attr('y', d => yScale(d.healthcare))
+        .text(d => d.abbr)
+        .classed('stateText', true);
 
-chartGroup.append('text')
-        .attr('transform', `translate(${chartWidth/2},${chartHeight + margin.top + 15})`)
-        .classed('aText', true)
-        .text('In Poverty (%)');
+
+
+        // Step 10: Create axes labels
+        chartGroup.append('text')
+                .attr('transform', 'rotate(-90)')
+                .attr('y', 0 - margin.left + 15)
+                .attr('x', 0 - (chartHeight/2))
+                .classed('aText', true)
+                .text('Lacks Healthcare (%)');
+
+        chartGroup.append('text')
+                .attr('transform', `translate(${chartWidth/2},${chartHeight + margin.top + 15})`)
+                .classed('aText', true)
+                .text('In Poverty (%)');
+
 }).catch(function(error){
     console.log(error);
 });
